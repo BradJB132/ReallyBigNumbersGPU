991c4277b51f65b99ad0ec446ffe1837c778f209
BradJB132 <bbaudi2@lsu.edu>
2023-04-13 18:53:59 -0500

    ILGPU it is then

--- Main.cs
+++ Main.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ILGPU;
+using ILGPU.Runtime;
+using ILGPU.Runtime.CPU;
+using ILGPU.Runtime.Cuda;
+using ILGPU.Runtime.OpenCL;
+using System;
+using System.IO;
+
+namespace ReallyBigNumbersGPU
+{
+    public class Test
+    {
+        public static void Main(string[] args)
+        {
+            // Builds a context that has all possible accelerators.
+            //using Context context = Context.CreateDefault();
+
+            // Builds a context that only has CPU accelerators.
+            //using Context context = Context.Create(builder => builder.CPU());
+
+            // Builds a context that only has Cuda accelerators.
+            //using Context context = Context.Create(builder => builder.Cuda());
+
+            // Builds a context that only has OpenCL accelerators.
+            //using Context context = Context.Create(builder => builder.OpenCL());
+
+            // Builds a context with only OpenCL and Cuda acclerators.
+            using Context context = Context.Create(builder =>
+            {
+                builder
+                    .OpenCL()
+                    .Cuda();
+            });
+
+            // Prints all accelerators.
+            foreach (Device d in context)
+            {
+                using Accelerator accelerator = d.CreateAccelerator(context);
+                Console.WriteLine(accelerator);
+                Console.WriteLine(GetInfoString(accelerator));
+            }
+
+            // Prints all CPU accelerators.
+            foreach (CPUDevice d in context.GetCPUDevices())
+            {
+                using CPUAccelerator accelerator = (CPUAccelerator)d.CreateAccelerator(context);
+                Console.WriteLine(accelerator);
+                Console.WriteLine(GetInfoString(accelerator));
+            }
+
+            // Prints all Cuda accelerators.
+            foreach (Device d in context.GetCudaDevices())
+            {
+                using Accelerator accelerator = d.CreateAccelerator(context);
+                Console.WriteLine("Cuda: " + accelerator);
+                Console.WriteLine(GetInfoString(accelerator));
+            }
+
+            // Prints all OpenCL accelerators.
+            foreach (Device d in context.GetCLDevices())
+            {
+                using Accelerator accelerator = d.CreateAccelerator(context);
+                Console.WriteLine("OpenCL: " + accelerator);
+                Console.WriteLine(GetInfoString(accelerator));
+            }
+        }
+
+        private static string GetInfoString(Accelerator a)
+        {
+            StringWriter infoString = new StringWriter();
+            a.PrintInformation(infoString);
+            return infoString.ToString();
+        }
+
+    }
+}

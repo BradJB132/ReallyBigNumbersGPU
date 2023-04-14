76a8c2ad183e79f264ce5a3a32701186cc868687
BradJB132 <bbaudi2@lsu.edu>
2023-04-13 19:11:45 -0500

    gpus are starting to look interesting

--- Main.cs
+++ Main.cs
@@ -25,44 +25,11 @@ namespace ReallyBigNumbersGPU
             //using Context context = Context.Create(builder => builder.OpenCL());
 
             // Builds a context with only OpenCL and Cuda acclerators.
-            using Context context = Context.Create(builder =>
-            {
-                builder
-                    .OpenCL()
-                    .Cuda();
-            });
+            Context context = Context.CreateDefault();
+            Accelerator accelerator = context.GetPreferredDevice(preferCPU: false)
+                                      .CreateAccelerator(context);
 
-            // Prints all accelerators.
-            foreach (Device d in context)
-            {
-                using Accelerator accelerator = d.CreateAccelerator(context);
-                Console.WriteLine(accelerator);
-                Console.WriteLine(GetInfoString(accelerator));
-            }
-
-            // Prints all CPU accelerators.
-            foreach (CPUDevice d in context.GetCPUDevices())
-            {
-                using CPUAccelerator accelerator = (CPUAccelerator)d.CreateAccelerator(context);
-                Console.WriteLine(accelerator);
-                Console.WriteLine(GetInfoString(accelerator));
-            }
-
-            // Prints all Cuda accelerators.
-            foreach (Device d in context.GetCudaDevices())
-            {
-                using Accelerator accelerator = d.CreateAccelerator(context);
-                Console.WriteLine("Cuda: " + accelerator);
-                Console.WriteLine(GetInfoString(accelerator));
-            }
-
-            // Prints all OpenCL accelerators.
-            foreach (Device d in context.GetCLDevices())
-            {
-                using Accelerator accelerator = d.CreateAccelerator(context);
-                Console.WriteLine("OpenCL: " + accelerator);
-                Console.WriteLine(GetInfoString(accelerator));
-            }
+            Console.WriteLine(GetInfoString(accelerator));
         }
 
         private static string GetInfoString(Accelerator a)

e2637423b0d572bee8c47f30bf8bf481d7a5cac2
BradJB132 <bbaudi2@lsu.edu>
2023-04-14 09:15:43 -0500

    update

--- Main.cs
+++ Main.cs
@@ -12,19 +12,7 @@ namespace ReallyBigNumbersGPU
     {
         public static void Main(string[] args)
         {
-            // Builds a context that has all possible accelerators.
-            //using Context context = Context.CreateDefault();
-
-            // Builds a context that only has CPU accelerators.
-            //using Context context = Context.Create(builder => builder.CPU());
-
-            // Builds a context that only has Cuda accelerators.
-            //using Context context = Context.Create(builder => builder.Cuda());
-
-            // Builds a context that only has OpenCL accelerators.
-            //using Context context = Context.Create(builder => builder.OpenCL());
-
-            // Builds a context with only OpenCL and Cuda acclerators.
+            
             Context context = Context.CreateDefault();
             Accelerator accelerator = context.GetPreferredDevice(preferCPU: false)
                                       .CreateAccelerator(context);
